e1d4e6164186c4b0f4a4664afac720365a4c8577
Gonzalez Matias Enrique <69493301+matttybu2020@users.noreply.github.com>
2022-07-26 00:16:29 -0300

    2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-
+import Logo from "../src/style/img/Logo.gif";
+import logo2 from "../src/style/img/logo2.gif"
+import logo3 from "../src/style/img/logo3.gif"
+import Rick from '../src/style/audio/Rick.mp3'
+import style from "../src/style/App.module.css";
 function App() {
   return (
+    <>
     <div className="App">
-      <h1>Henry Pokemon</h1>
+    <img className={style.logo} src={Logo} alt="" />
     </div>
+    <div className="App">
+    <img className={style.logo} src={logo2 } alt="" />
+    </div>
+    <div className="App">
+    <img className={style.logo} src={logo3 } alt="" />
+    </div>
+    <div>
+    <audio src={Rick}></audio>
+    </div>
+   
+    </>
   );
 }
 
